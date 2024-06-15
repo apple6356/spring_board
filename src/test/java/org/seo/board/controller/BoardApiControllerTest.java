226fb4ac97aa148b2d7f92b45006a39fe5a45db0
apple6356 <apple1418@naver.com>
2024-06-15 16:08:52 +0900

    24-06-15

--- src/test/java/org/seo/board/controller/BoardApiControllerTest.java
+++ src/test/java/org/seo/board/controller/BoardApiControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -39,6 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class BoardApiControllerTest {
 
     @Autowired
