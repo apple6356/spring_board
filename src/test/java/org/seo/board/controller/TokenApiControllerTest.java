292d90717dc8eea825dc42aed98a67362684d23e
apple6356 <apple1418@naver.com>
2024-06-15 16:19:41 +0900

    24-06-15

--- src/test/java/org/seo/board/controller/TokenApiControllerTest.java
+++ src/test/java/org/seo/board/controller/TokenApiControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -29,7 +28,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class TokenApiControllerTest {
     @Autowired
     protected MockMvc mockMvc;
