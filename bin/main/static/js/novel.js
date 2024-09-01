d0f03ae8135c7f83d6f4a79043674f2424419551
apple6356 <apple1418@naver.com>
2024-09-01 22:55:43 +0900

    24-09-01

--- bin/main/static/js/novel.js
+++ bin/main/static/js/novel.js
@@ -32,7 +32,7 @@ function httpRequest(method, url, body, success, fail) {
     })
         .then((response) => {
             if (response.status === 200 || response.status === 201) {
-                return success();
+                return response.json();
             }
             const refresh_token = getCookie("refresh_token");
             if (response.status === 401 && refresh_token) {
@@ -56,6 +56,10 @@ function httpRequest(method, url, body, success, fail) {
             } else {
                 return fail();
             }
+        })
+        .then(data => {
+            // if (data) 는 데이터가 truthy한 값인지 확인, null, undefined, 0, 빈 문자열, false 등인지 확인
+            if (data) success(data);
         });
 }
 
@@ -464,110 +468,148 @@ if (novelSearchButton) {
     });
 }
 
-// // 댓글 작성
-// const commentCreateButton = document.getElementById('comment-create-btn');
+// 댓글 리스트 갱신
+function updateCommentList(data) {
+    const commentList = document.getElementById('comment-list');
+    commentList.innerHTML = '';
 
-// if (commentCreateButton) {
-//     commentCreateButton.addEventListener('click', event => {
-//         chapterId = document.getElementById('chapter-id').value;
+    console.log("data: " + data);
 
-//         body = JSON.stringify({
-//             chapterId: chapterId,
-//             content: document.getElementById('comment-content').value
-//         });
+    data.forEach(comment => {
+        const commentDiv = document.createElement('div');
+        const currentUser = document.getElementById('current-user').value;
 
-//         function success(data) {
-//             alert('댓글이 작성되었습니다.');
-//             location.replace('/novelView?id=' + chapterId);
-//             // updateCommentList(data);
-//             // document.getElementById('comment-content').value = '';
-//         };
-//         function fail() {
-//             alert('댓글 작성에 실패했습니다.');
-//         };
+        console.log("currentUser: " + currentUser);
+        console.log("comment.author: " + comment.author);
 
-//         httpRequest('POST', '/api/chapter-comments', body, success, fail);
+        commentDiv.innerHTML = `
+            <div>작성자 : ${comment.author}, 작성 시간 : ${new Date(comment.createdAt).toLocaleString()}</div>
 
-//     });
-// }
+            <button type="button" onclick="commentRecommend(${comment.id})">추천</button>
 
-// // 댓글 수정 화면
-// function commentModify(commentId, button) {
-//     let commentParent = button.parentElement; // 부모 div
-//     let commentContent = document.getElementById('comment-content-' + commentId); // comment-content 가져오기
+            <span id="comment-recommend-${comment.id}">${comment.recommend}</span> <br>
 
-//     let textarea = document.createElement('textarea'); // textarea 태그 추가
-//     textarea.value = commentContent.innerText; // textarea 에 기존 내용 삽입
+            <div id="comment-content-${comment.id}">${comment.content}</div>
 
-//     commentParent.replaceChild(textarea, commentContent); // 기존 div를 댓글 수정을 위해 textarea로 대체
+            ${currentUser === comment.author ? `
+                <button type="button" id="modify-comment-btn-${comment.id}" onclick="commentModify(${comment.id}, this)">수정</button>
+                <button type="button" id="delete-comment-btn-${comment.id}" onclick="commentDelete(${comment.id})">삭제</button>
+            ` : ''}
+        `;
+        commentList.appendChild(commentDiv);
+        commentList.appendChild(document.createElement('hr'));
+    });
+}
 
-//     let commentDeleteButton = document.getElementById('delete-comment-btn-' + commentId);
-//     commentDeleteButton.style.display = 'none'; // 삭제 버튼 안보이게
+// 댓글 작성
+const commentCreateButton = document.getElementById('comment-create-btn');
 
-//     button.innerText = '등록'; // 버튼 등록으로 변경
+if (commentCreateButton) {
+    commentCreateButton.addEventListener('click', event => {
+        chapterId = document.getElementById('chapter-id').value;
 
-//     button.onclick = function () { // 등록 버튼 누르면 댓글 수정
-//         updateComment(commentId, textarea.value, button);
-//     }
-// }
+        body = JSON.stringify({
+            chapterId: chapterId,
+            content: document.getElementById('comment-content').value
+        });
 
-// // 댓글 수정
-// function updateComment(commentId, commentContent, button) {
-//     chapterId = document.getElementById('chapter-id').value;
+        function success(data) {
+            alert('댓글이 작성되었습니다.');
+            // location.replace('/novelView?id=' + chapterId);
+            updateCommentList(data);
+            document.getElementById('comment-content').value = '';
+        };
+        function fail() {
+            alert('댓글 작성에 실패했습니다.');
+        };
 
-//     body = JSON.stringify({
-//         content: commentContent,
-//     });
+        httpRequest('POST', '/api/chapter-comments', body, success, fail);
 
-//     function success() {
-//         alert("수정 완료");
-//         location.replace('/novelView?id=' + chapterId);
-//     }
+    });
+}
 
-//     function fail() {
-//         alert("수정 실패");
-//     }
+// 댓글 수정 화면
+function commentModify(commentId, button) {
+    let commentParent = button.parentElement; // 부모 div
+    let commentContent = document.getElementById('comment-content-' + commentId); // comment-content 가져오기
 
-//     httpRequest("PUT", "/api/chapter-comments/" + commentId, body, success, fail);
+    let textarea = document.createElement('textarea'); // textarea 태그 추가
+    textarea.value = commentContent.innerText; // textarea 에 기존 내용 삽입
 
-// }
+    commentParent.replaceChild(textarea, commentContent); // 기존 div를 댓글 수정을 위해 textarea로 대체
 
-// // 댓글 삭제
-// function commentDelete(commentId) {
+    let commentDeleteButton = document.getElementById('delete-comment-btn-' + commentId);
+    commentDeleteButton.style.display = 'none'; // 삭제 버튼 안보이게
 
-//     if (confirm("삭제 후 복구할 수 없습니다. \n 정말 삭제하시겠습니까?")) {
-//         let chapterId = document.getElementById('chapter-id').value; // board 의 id 저장
+    button.innerText = '등록'; // 버튼 등록으로 변경
 
-//         function success() {
-//             alert("삭제 완료");
-//             location.replace('/novelView?id=' + chapterId);
-//         }
-//         function fail() {
-//             alert("삭제 실패");
-//         }
+    button.onclick = function () { // 등록 버튼 누르면 댓글 수정
+        updateComment(commentId, textarea.value, button);
+    }
+}
 
-//         httpRequest("DELETE", "/api/chapter-comments/" + commentId, null, success, fail);
-//     } else {
-//         return;
-//     }
+// 댓글 수정
+function updateComment(commentId, commentContent, button) {
+    chapterId = document.getElementById('chapter-id').value;
 
-// }
+    body = JSON.stringify({
+        content: commentContent,
+    });
 
-// // 댓글 추천+1
-// function commentRecommend(commentId) {
-//     let chapterId = document.getElementById('chapter-id').value;
+    function success(data) {
+        alert("수정 완료");
+        // location.replace('/novelView?id=' + chapterId);
+        updateCommentList(data);
+        // document.getElementById('comment-content').value = '';
+    }
 
-//     function success() {
-//         alert("추천 완료");
-//         location.replace('/novelView?id=' + chapterId);
-//     }
-//     function fail() {
-//         alert("추천 실패");
-//     }
+    function fail() {
+        alert("수정 실패");
+    }
 
-//     httpRequest("PUT", "/api/chapter-comments-recommend/" + commentId, null, success, fail);
+    httpRequest("PUT", "/api/chapter-comments/" + commentId, body, success, fail);
 
-// }
+}
+
+// 댓글 삭제
+function commentDelete(commentId) {
+
+    if (confirm("삭제 후 복구할 수 없습니다. \n 정말 삭제하시겠습니까?")) {
+        let chapterId = document.getElementById('chapter-id').value; // board 의 id 저장
+
+        function success(data) {
+            alert("삭제 완료");
+            // location.replace('/novelView?id=' + chapterId);
+            updateCommentList(data);
+        }
+        function fail() {
+            alert("삭제 실패");
+        }
+
+        httpRequest("DELETE", "/api/chapter-comments/" + commentId, null, success, fail);
+    } else {
+        return;
+    }
+
+}
+
+// 댓글 추천+1
+function commentRecommend(commentId) {
+    let chapterId = document.getElementById('chapter-id').value;
+
+    function success(data) {
+        alert("추천 완료");
+        // location.replace('/novelView?id=' + chapterId);
+        updateCommentList(data);
+    }
+    function fail() {
+        alert("추천 실패");
+    }
+
+    httpRequest("PUT", "/api/chapter-comments-recommend/" + commentId, null, success, fail);
+
+}
+
 
 // 회차 로드
 // function loadChapter(chapterId) {
